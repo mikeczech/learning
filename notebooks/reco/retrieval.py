a72908afe0c578179236415593d06af50df9d463
Mike Czech <zenscr@gmail.com>
2024-01-25 18:16:11 +0100

    Format

--- notebooks/reco/retrieval.py
+++ notebooks/reco/retrieval.py
@@ -5,17 +5,21 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class QueryTower(nn.Module):
 
-    def __init__(self, user_ids: List[str], user_emb_dim: int= 16):
+class QueryTower(nn.Module):
+    def __init__(
+        self, user_ids: List[str], user_emb_dim: int = 16, output_dim: int = 10
+    ):
         super(QueryTower, self).__init__()
 
         self.user_id_to_index = {user_id: i for i, user_id in enumerate(user_ids)}
         self.user_emb = nn.Embedding(len(user_ids), user_emb_dim)
         self.normalized_age = nn.BatchNorm1d(1)
-        self.linear = nn.Linear(user_emb_dim + 1, user_emb_dim)
+        self.linear = nn.Linear(
+            user_emb_dim + 1, output_dim
+        )  # what woulde be a good target dimension?
 
-    def forward(self, customer_ids: List["str"], ages: torch.Tensor):
+    def forward(self, customer_ids: List[str], ages: torch.Tensor):
         user_indices = self.get_user_indices(customer_ids)
         user_features = self.user_emb(user_indices)
 
@@ -25,12 +29,13 @@ class QueryTower(nn.Module):
 
         return self.linear(features)
 
-    def get_user_indices(self, user_ids):
-        return torch.tensor([self.user_id_to_index[user_id] for user_id in user_ids], dtype=torch.long)
+    def get_user_indices(self, user_ids: List[str]):
+        return torch.tensor(
+            [self.user_id_to_index[user_id] for user_id in user_ids], dtype=torch.long
+        )
 
 
 class ItemTower(nn.Module):
-
     def __init__(self):
         pass
 
